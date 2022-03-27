7e865c4b48d346fda5510f26452204992ab4893f
Elwes_Honorato <elweshonorato@gmail.com>
2022-03-27 14:52:00 -0300

    Correction of job_spark.py path

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+# Centralize Terraform control file
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-igti-elwes"
+        key    = "state/igti/edc/mod1/terraform.tfstate"
+        region = "us-east-2"
+    }
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,12 +13,3 @@ variable "numero_conta" {
 variable "aws_region" {
     default = "us-east-2"
 }
-
-# Centralize Terraform control file
-terraform {
-    backend "s3" {
-        bucket = "terraform-state-igti-elwes"
-        key    = "state/igti/edc/mod1/terraform.tfstate"
-        region = "us-east-2"
-    }
-}
